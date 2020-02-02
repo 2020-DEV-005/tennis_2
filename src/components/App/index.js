b69d4971c268c7db791e82d9ed49d4eac3349292
2020-DEV-005 <2020.dev.005@gmail.com>
2020-02-02 16:35:53 +0000

    Title added to the application

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {AppConst} from '../../constants/App.const.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>{AppConst.TITLE}</h2>
       </header>
     </div>
   );
---
+++ src/components/App/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from './';
+import {AppConst} from '../../constants/App.const';
+
+describe("<App/> component", () => {
+  let wrapper = shallow(<App/>);
+  it("The application should have the correct title", () => {
+    expect(wrapper.find("header h2").text()).toEqual(AppConst.TITLE);
+  });
+});
