993feb235f6b173fa9774a9f258674ed5c0ed664
2020-DEV-005 <2020.dev.005@gmail.com>
2020-02-02 16:45:59 +0000

    Removed the duplicate code of player selector

--- src/components/Game/index.test.js
+++ src/components/Game/index.test.js
@@ -6,8 +6,9 @@ import {AppConst} from '../../constants/App.const';
 describe("<Game /> component", () => {
     let wrapper = shallow(<Game />);
     it("Should have label for each player", () => {
-        expect(wrapper.find("div.player h4").length).toEqual(2);
-        expect(wrapper.find("div.player h4").at(0).text()).toEqual(AppConst.PLAYER_1);
-        expect(wrapper.find("div.player h4").at(1).text()).toEqual(AppConst.PLAYER_2);
+        let playerList = wrapper.find("div.player h4");
+        expect(playerList.length).toEqual(2);
+        expect(playerList.at(0).text()).toEqual(AppConst.PLAYER_1);
+        expect(playerList.at(1).text()).toEqual(AppConst.PLAYER_2);
     });
 });
